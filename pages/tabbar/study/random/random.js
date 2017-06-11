333ffc6ac242e73661b0813ff617ce74cbd798bf
MrTsao <rcode@163.com>
2017-06-11 23:06:03 +0800

    update random page

--- pages/tabbar/study/random/random.js
+++ pages/tabbar/study/random/random.js
@@ -5,60 +5,62 @@ var app = getApp()
 Page({
   data: {
     userInfo: {},
-    PAGE: "RANDOM",
-    exerises: [{
-      id: 'ID_TITLE1'
-      , type: '0'
-      , title: '下列有关公民权利能力的表述，有哪一项是错误的？'
-      , options: [{ op: 'A', text: '权利能力是公民构成法律关系主体的一种资格' }
-        , { op: 'B', text: '所有公民的权利能力都是相同的' }
-        , { op: 'C', text: '公民具有权利能力，并不必然具有行为能力' }
-        , { op: 'D', text: '权利能力也包括公民承担义务的能力或资格' }]
-      //题目答案
-      , answer: 'B'
-      //用户选择
-      , u_answer: ''
-      , isAnswered: false
-      //标签、关键字
-      , tag: [{ op: '2016真题' }, { op: '卷一内容' }, { op: '宪法' }]
-      //关联法条
-      , rlaw: [{ id: 'id1', op: 'test1', de: 'test2', ve: 'test3', co: 'test4' }, { id: 'id2', op: 'tse1', de: 'tse1', ve: 'tse1', co: 'tse1' }]
-      //关联考纲
-      , rsub: [{ id: 'id1', year: '2017', op: 'sfsd2' }, { id: 'id2', year: '2017', op: 'sfsd2' }]
-      //当前显示项
-      , show_item: 0
-      //解析
-      , desc: '首先要明确什么是公民。我国《宪法》第33条第一款对公民的内涵作了明确的规定：“凡具有中华人民共和国国籍的人都是中华人民共和国的公民。”这就是说，在我国，公民资格是与国籍分不开的。 由于我国罪犯受到刑罚制裁并不导致其丧失我国国籍的法律后果，因此他们仍然具有我国的国籍，是我国的公民。 其次又应当看到，罪犯作为公民，依法享有公民的权利，但由于他们受到刑罚制裁，其权利状况因此发生明显改变，即罪犯不能享有法律规定的全部公民权利，因为任何一种刑罚方法都是对犯罪人某种权利的剥夺或限制，因此其法律地位又区别于其他公民。而他们的某些未被剥夺或限制的权利因无法以自己的行为实现而在实际上处于中止的状态，比如罪犯不可能行使受全日制高等教育权、正常夫妻生活权、对未成年子女抚养权，不可能进行经商、炒股、旅游等活动，而未被剥夺政治权利的罪犯也不可能实施集会、结社、游行、示威等行为。'
-      //评论
-      , feeds: [{ id: '1', user: 'Lucy', head_img: '/image/Female50.png', feed_txt: '不小心选错了... ', isAgreed: false, agree: 2, comments: 4, time: '1天前' },
-      { id: '2', user: 'Jack', head_img: '/image/Male50.png', feed_txt: '不小心选错了... ', isAgreed: true, agree: 2, comments: 4, time: '2个星期前' },
-      { id: '3', user: 'Jim', head_img: '/image/Male50.png', feed_txt: '不小心选错了... ', isAgreed: false, agree: 2, comments: 4, time: '1个月前' }]
-      //是否收藏
-      , is_coll: false
+    week: ["", "", ""],
+    items: [{
+      txt: "练习量",
+      val: "5",
+      color: "#cd853f",
+      img: "/image/exam.png"
     }, {
-      id: 'ID_TITLE2'
-      , type: '1'
-      , title: '学者们认为，法律不是万能的，其作用是有限的，其理由在于：①法律重视程序，不讲效率；②法律调整外在行为，不干预人的思想观念；③法律强调稳定性，避免灵活性；④法律反映客观规律，不体现人的意志。下列哪些选项是正确的？'
-      , options: [{ op: 'A', text: '④③①' }
-        , { op: 'B', text: '①④②' }
-        , { op: 'C', text: '②' }
-        , { op: 'D', text: '③' }]
-      , answer: 'CD'
-      , u_answer: ''
-      , tag: [{ op: '2015真题' }, { op: '卷一内容' }]
-      , show_item: 0
-      , desc: '。'
-      , feeds: [{ id: '1', user: 'Lucy', head_img: '/image/Female50.png', feed_txt: '........ ', isAgreed: true, agree: 2, comments: 4, time: '1天前' },
-      { id: '2', user: 'Jack', head_img: '/image/Male50.png', feed_txt: '....... ', isAgreed: false, agree: 2, comments: 4, time: '2个星期前' },
-      { id: '3', user: 'Jim', head_img: '/image/Male50.png', feed_txt: '....... ', isAgreed: false, agree: 2, comments: 4, time: '1个月前' }]
-      , is_coll: true
+      txt: "覆盖率",
+      val: "0.5%",
+      color: "#008B8B",
+      img: "/image/crate.png"
+    }, {
+      txt: "总用时",
+      val: "10分钟",
+      color: "#3CB371",
+      img: "/image/time.png"
     }],
+    PAGE: "RANDOM",
+    exerises: [],
+    // exerises: [{
+    //   id: 'ID_TITLE1'
+    //   , type: '0'
+    //   , title: '下列有关公民权利能力的表述，有哪一项是错误的？'
+    //   , options: [{ op: 'A', text: '权利能力是公民构成法律关系主体的一种资格' }
+    //     , { op: 'B', text: '所有公民的权利能力都是相同的' }
+    //     , { op: 'C', text: '公民具有权利能力，并不必然具有行为能力' }
+    //     , { op: 'D', text: '权利能力也包括公民承担义务的能力或资格' }]
+    //   //题目答案
+    //   , answer: 'B'
+    //   //用户选择
+    //   , u_answer: ''
+    //   , isAnswered: false
+    //   //标签、关键字
+    //   , tag: [{ op: '2016真题' }, { op: '卷一内容' }, { op: '宪法' }]
+    //   //关联法条
+    //   , rlaw: [{ id: 'id1', op: 'test1', de: 'test2', ve: 'test3', co: 'test4' }, { id: 'id2', op: 'tse1', de: 'tse1', ve: 'tse1', co: 'tse1' }]
+    //   //关联考纲
+    //   , rsub: [{ id: 'id1', year: '2017', op: 'sfsd2' }, { id: 'id2', year: '2017', op: 'sfsd2' }]
+    //   //当前显示项
+    //   , show_item: 0
+    //   //解析
+    //   , desc: '首先要明确什么是公民。我国《宪法》第33条第一款对公民的内涵作了明确的规定：“凡具有中华人民共和国国籍的人都是中华人民共和国的公民。”这就是说，在我国，公民资格是与国籍分不开的。 由于我国罪犯受到刑罚制裁并不导致其丧失我国国籍的法律后果，因此他们仍然具有我国的国籍，是我国的公民。 其次又应当看到，罪犯作为公民，依法享有公民的权利，但由于他们受到刑罚制裁，其权利状况因此发生明显改变，即罪犯不能享有法律规定的全部公民权利，因为任何一种刑罚方法都是对犯罪人某种权利的剥夺或限制，因此其法律地位又区别于其他公民。而他们的某些未被剥夺或限制的权利因无法以自己的行为实现而在实际上处于中止的状态，比如罪犯不可能行使受全日制高等教育权、正常夫妻生活权、对未成年子女抚养权，不可能进行经商、炒股、旅游等活动，而未被剥夺政治权利的罪犯也不可能实施集会、结社、游行、示威等行为。'
+    //   //评论
+    //   , feeds: [{ id: '1', user: 'Lucy', head_img: '/image/Female50.png', feed_txt: '不小心选错了... ', isAgreed: false, agree: 2, comments: 4, time: '1天前' },
+    //   { id: '2', user: 'Jack', head_img: '/image/Male50.png', feed_txt: '不小心选错了... ', isAgreed: true, agree: 2, comments: 4, time: '2个星期前' },
+    //   { id: '3', user: 'Jim', head_img: '/image/Male50.png', feed_txt: '不小心选错了... ', isAgreed: false, agree: 2, comments: 4, time: '1个月前' }]
+    //   //是否收藏
+    //   , is_coll: false
+    // }],
     index: 0,
     right: 0,
     error: 0,
     comm_text: '',
     comm_len: 0,
-    showModalStatus: false
+    show_comment_module: false,
+    show_start_module: true
   },
   selected: function (e) {
     sUtil.selectedOptions(e, this)
@@ -98,6 +100,13 @@ Page({
   doComments: function (e) {
     this.showModal()
   },
+  startTran: function (e) {
+    //开始练习
+    this.setData({
+      show_start_module: false
+    })
+    console.log(e)
+  },
   SubmitComm: function (e) {
     var that = this
     let iIndex = that.data.index
@@ -126,13 +135,13 @@ Page({
   showModal: function () {
     // 显示遮罩层
     this.setData({
-      showModalStatus: true
+      show_comment_module: true
     })
   },
   hideModal: function () {
     // 隐藏遮罩层
     this.setData({
-      showModalStatus: false
+      show_comment_module: false
     })
   },
   onLoad: function (options) {
@@ -142,6 +151,7 @@ Page({
       //更新数据
       that.setData({
         userInfo: userInfo
+        , week: util.currentWeekInfo()
       })
     })
     Post.call(this, this, "LOAD")
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,34 @@
 function formatTime(date) {
-    var year = date.getFullYear()
-    var month = date.getMonth() + 1
-    var day = date.getDate()
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
 
-    var hour = date.getHours()
-    var minute = date.getMinutes()
-    var second = date.getSeconds()
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
 
-
-    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 function formatNumber(n) {
-    n = n.toString()
-    return n[1] ? n : '0' + n
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+function currentWeekInfo() {
+  var now = new Date()
+  var nowDayOfWeek = now.getDay() //今天本周的第几天
+  var nowDay = now.getDate() //当前日
+  var nowMonth = now.getMonth() + 1 //当前月
+  var nowYear = now.getYear() //当前年
+  var weekStartDate = new Date(nowYear, nowMonth, nowDay - nowDayOfWeek)
+  var weekEndDate = new Date(nowYear, nowMonth, nowDay + (6 - nowDayOfWeek))
+  var sweek = formatNumber(weekStartDate.getMonth()) + '月' + formatNumber(weekStartDate.getDate()) + '日'
+  var eweek = formatNumber(weekEndDate.getMonth()) + '月' + formatNumber(weekEndDate.getDate()) + '日'
+
+  var num = Math.ceil((nowDay + 6 - nowDayOfWeek) / 7).toString();
+
+  return [sweek + ' ～ ' + eweek, nowMonth.toString(), num]
 }
 
 /**
@@ -23,21 +38,21 @@ function formatNumber(n) {
  */
 function _get(url, success, fail) {
 
-    console.log("------start---_get----");
-    wx.request({
-        url: url,
-        header: {
-            // 'Content-Type': 'application/json'
-        },
-        success: function (res) {
-            success(res);
-        },
-        fail: function (res) {
-            fail(res);
-        }
-    });
+  console.log("------start---_get----");
+  wx.request({
+    url: url,
+    header: {
+      // 'Content-Type': 'application/json'
+    },
+    success: function (res) {
+      success(res);
+    },
+    fail: function (res) {
+      fail(res);
+    }
+  });
 
-    console.log("----end-----_get----");
+  console.log("----end-----_get----");
 }
 
 /**
@@ -46,106 +61,106 @@ function _get(url, success, fail) {
 * fail 失败的回调
 */
 function _post_json(url, jsPost, success, fail) {
-    //console.log("----_post--start-------");
-    // wx.showToast({
-    //     title: "正在加载...",
-    //     icon: "loading",
-    //     duration: 5000
-    // })
-    wx.showNavigationBarLoading()
-    var user = wx.getStorageSync('user')
-    if (jsPost == null) jsPost = new jsonRow();
-    jsPost.AddCell("OPEN_ID", user.openid)
-    wx.request({
-        url: url,
-        header: {
-            'content-type': 'application/json',
-        },
-        method: 'POST',
-        data: jsPost.GetStr(),
-        success: function (res) {
-            //wx.hideToast()
-            wx.hideNavigationBarLoading()
-            if (res.data.msg == "err") {
-                console.log(res.data.data);
-            } else {
-                if (res.data.msg != "") {
-                    wx.showToast({
-                        title: res.data.msg || "错误"
-                    })
-                }
-                success(res);
-            }
-        },
-        fail: function (res) {
-            console.log(res);
+  //console.log("----_post--start-------");
+  // wx.showToast({
+  //     title: "正在加载...",
+  //     icon: "loading",
+  //     duration: 5000
+  // })
+  wx.showNavigationBarLoading()
+  var user = wx.getStorageSync('user')
+  if (jsPost == null) jsPost = new jsonRow();
+  jsPost.AddCell("OPEN_ID", user.openid)
+  wx.request({
+    url: url,
+    header: {
+      'content-type': 'application/json',
+    },
+    method: 'POST',
+    data: jsPost.GetStr(),
+    success: function (res) {
+      //wx.hideToast()
+      wx.hideNavigationBarLoading()
+      if (res.data.msg == "err") {
+        console.log(res.data.data);
+      } else {
+        if (res.data.msg != "") {
+          wx.showToast({
+            title: res.data.msg || "错误"
+          })
         }
-    });
-
-    console.log("----end----_post-----");
+        success(res);
+      }
+    },
+    fail: function (res) {
+      console.log(res);
+    }
+  });
+  //console.log("----end----_post-----");
 }
 
 function jsonRow() { this.arrjson = {}; }
 jsonRow.prototype = {
-    AddCell: function (sName, sValue) {
-        if (!sName) return; this.arrjson[sName] = sValue;
-    },
-    GetStr: function () {
-        var _strjson = "";
-        for (var m in this.arrjson) {
-            if (_strjson == "") { _strjson = "\"" + m + "\":\"" + this.arrjson[m] + "\""; } else { _strjson += ",\"" + m + "\":\"" + encodeURIComponent(this.arrjson[m]) + "\""; }
-        }
-        if (_strjson == "") return ""; else return "{" + _strjson + "}";
+  AddCell: function (sName, sValue) {
+    if (!sName) return; this.arrjson[sName] = sValue;
+  },
+  GetStr: function () {
+    var _strjson = "";
+    for (var m in this.arrjson) {
+      if (_strjson == "") { _strjson = "\"" + m + "\":\"" + this.arrjson[m] + "\""; } else { _strjson += ",\"" + m + "\":\"" + encodeURIComponent(this.arrjson[m]) + "\""; }
     }
+    if (_strjson == "") return ""; else return "{" + _strjson + "}";
+  }
 };
 
 
 function exerisesNext() {
-    //return exerises_next.next;
-    var next = {
-        "id": 2,
-        "data": [
-            {
-                "question_id": 9,
-                "answer_id": 61,
-                "feed_source_id": 25,
-                "feed_source_name": "George",
-                "feed_source_txt": "赞了回答9",
-                "feed_source_img": "../../images/icon9.jpeg",
-                "question": "气象铁塔的辐射大吗？",
-                "answer_ctnt": "我不知道那个铁塔的情况，不过气象铁塔上会有一些测太阳辐射的设备，如果说辐射的话，太阳辐射那么多，大家赶紧躲进地底下呀~~~~~要不然辐射量这么大，会变异的呀~~~~",
-                "good_num": "112",
-                "comment_num": "18"
-            }]
-    }
-    return next
+  //return exerises_next.next;
+  var next = {
+    "id": 2,
+    "data": [
+      {
+        "question_id": 9,
+        "answer_id": 61,
+        "feed_source_id": 25,
+        "feed_source_name": "George",
+        "feed_source_txt": "赞了回答9",
+        "feed_source_img": "../../images/icon9.jpeg",
+        "question": "气象铁塔的辐射大吗？",
+        "answer_ctnt": "我不知道那个铁塔的情况，不过气象铁塔上会有一些测太阳辐射的设备，如果说辐射的话，太阳辐射那么多，大家赶紧躲进地底下呀~~~~~要不然辐射量这么大，会变异的呀~~~~",
+        "good_num": "112",
+        "comment_num": "18"
+      }]
+  }
+  return next
 }
 
 /**
   * 旋转刷新图标
   */
 function updateRefreshIcon() {
-    var deg = 0;
-    console.log('旋转开始了.....')
-    var animation = wx.createAnimation({
-        duration: 1000
-    });
+  var deg = 0;
+  console.log('旋转开始了.....')
+  var animation = wx.createAnimation({
+    duration: 1000
+  });
 
-    var timer = setInterval(() => {
-        if (!this.data.loading)
-            clearInterval(timer);
-        animation.rotateZ(deg).step();//在Z轴旋转一个deg角度
-        deg += 360;
-        this.setData({
-            refreshAnimation: animation.export()
-        })
-    }, 2000);
+  var timer = setInterval(() => {
+    if (!this.data.loading)
+      clearInterval(timer);
+    animation.rotateZ(deg).step();//在Z轴旋转一个deg角度
+    deg += 360;
+    this.setData({
+      refreshAnimation: animation.export()
+    })
+  }, 2000);
 }
 
 module.exports = {
-    formatTime: formatTime,
-    _get: _get,
-    _post: _post_json,
-    jsonRow: jsonRow,
-    exerisesNext: exerisesNext
+  formatTime: formatTime,
+  currentWeekInfo: currentWeekInfo,
+  _get: _get,
+  _post: _post_json,
+  jsonRow: jsonRow,
+  exerisesNext: exerisesNext
 }
