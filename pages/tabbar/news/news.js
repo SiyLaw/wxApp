f850c04e3c62f9448d99b2f2b7818daaf8ec617c
MrTsao <rcode@163.com>
2017-07-30 21:56:09 +0800

    change news page

--- pages/tabbar/news/news.js
+++ pages/tabbar/news/news.js
@@ -1,19 +1,27 @@
 // pages/news/news.js
-var util = require('../../../utils/util.js')
+var util = require('../../../utils/util.js');
 var app = getApp();
+var startDot = { x: 0, y: 0 };//触摸时的原点
+var endDot = { x: 0, y: 0 };
 Page({
   data: {
     height: 0,
     width: 0,
-    bseurl:'',
+    bseurl: '',
     rpxrate: 0.0, //rpx-px比率
     PAGE: "NEWS_LIST",
+    RCNT: 10,//每次请求数量
     HOTS: [],
     second: 5,//录音秒
     finished: true,//是否完成录音
     searchval: "",//搜索内容
     menuhide: true,//是否折叠菜单
-    menu: []//菜单内容
+    menu: [],//菜单内容
+    menuIndex: -1,
+    menuKey: '', //内容分类浏览关键字
+    moreLoading: false,
+    moreLoadingComplete: false,
+    currenttime: 0,
   },
   onPullDownRefresh() {
     wx.stopPullDownRefresh()
@@ -24,12 +32,17 @@ Page({
     var height = 0;
     var width = 0;
     var SysInfo = wx.getSystemInfoSync()
-    Post.call(this, this, "LOAD", null, function (that, data) {
+
+    var jsPost = new util.jsonRow()
+    jsPost.AddCell("RCNT", that.data.RCNT)
+    jsPost.AddCell("CCNT", that.data.HOTS.length)
+    Post.call(this, this, "LOAD", jsPost, function (that, data) {
       that.setData({
         height: SysInfo.windowHeight,
         width: SysInfo.screenWidth,
         rpxrate: Math.floor(SysInfo.screenWidth / 750 * 100) / 100,
         HOTS: data.HOTS,
+        menu: data.MENU,
         bseurl: app.globalData.bseurl
       })
     });
@@ -40,6 +53,49 @@ Page({
       menuhide: !this.data.menuhide
     })
   },
+  subitemcontrol: function (e) {
+    let key = ''
+    if (e.currentTarget.dataset.idx != -1)
+      key = this.data.menu[e.currentTarget.dataset.idx].id
+    this.setData({
+      menuIndex: e.currentTarget.dataset.idx
+      , menuhide: !this.data.menuhide
+      , menuKey: key
+    });
+  },
+  onPullDownRefresh() {
+    var jsPost = new util.jsonRow()
+    jsPost.AddCell("RCNT", this.data.HOTS.length)
+    jsPost.AddCell("CCNT", 0)
+    Post.call(this, this, "LOAD", jsPost, function (that, data) {
+      that.setData({
+        HOTS: data.HOTS,
+        moreLoadingComplete: false
+      })
+    });
+    wx.stopPullDownRefresh()
+  },
+  onReachBottom: function (e) {
+    if (!this.data.moreLoadingComplete) {
+      this.setData({
+        moreLoading: true
+      })
+
+      var jsPost = new util.jsonRow()
+      jsPost.AddCell("RCNT", this.data.RCNT)
+      jsPost.AddCell("CCNT", this.data.HOTS.length)
+      Post.call(this, this, "MORE", jsPost, function (that, data) {
+        let loadComplete = false;
+        if (data.HOTS.length < 1)
+          loadComplete = true;
+        that.setData({
+          HOTS: that.data.HOTS.concat(data.HOTS),
+          moreLoading: false,
+          moreLoadingComplete: true
+        })
+      })
+    }
+  }
 })
 //服务器请求数据
 function Post(that, action, data, doAfter) {
